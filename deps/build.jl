66e2223ceafb3e754b74f3b5aafa0336eb8df390
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-06-26 12:30:53 -0500

    use Pkg.add the registered versions

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,8 @@
 using Pkg
 Pkg.activate()
-rootdir = dirname(@__DIR__)
-Pkg.develop([
-  PackageSpec(path=joinpath(rootdir,"SnoopCompileCore")),
-  PackageSpec(path=joinpath(rootdir,"SnoopCompileAnalysis")),
-  PackageSpec(path=joinpath(rootdir,"SnoopCompileBot")),
-  PackageSpec(path=rootdir),
+Pkg.add([
+  "SnoopCompileCore",
+  "SnoopCompileAnalysis",
+  "SnoopCompileBot",
 ])
 Pkg.resolve()
